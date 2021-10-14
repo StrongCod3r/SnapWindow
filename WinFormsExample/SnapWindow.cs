0466f77d39c0fd0aaa9304d52e57943d685829ea
sc <sc@DESKTOP-3A2RNP7>
2021-10-15 01:09:11 +0200

    Add project files.

---
+++ WinFormsExample/SnapWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinFormsExample
+{
+    public class SnapWindow
+    {
+        private Form form;
+        private IntPtr handle;
+        public uint SnapDist = 50;
+
+        public SnapWindow(Form form)
+        {
+            this.form = form;
+            this.handle = form.Handle;
+            this.form.ResizeEnd += Form_ResizeEnd;
+        }
+
+        private bool DoSnap(int pos, int edge)
+        {
+            int delta = pos - edge;
+            return delta > 0 && delta <= SnapDist;
+        }
+
+        private void Form_ResizeEnd(object sender, EventArgs e)
+        {
+            var winRect = WinApi.GetWindowRectangle(this.handle);
+            Screen screen = Screen.FromPoint(this.form.Location);
+
+            int widthReal = winRect.Right - winRect.Left;
+            int heightReal = winRect.Bottom - winRect.Top;
+
+            if (DoSnap(winRect.Left, screen.WorkingArea.Left))
+                this.form.Left = widthReal - this.form.Width;
+            if (DoSnap(winRect.Top, screen.WorkingArea.Top))
+                this.form.Top = screen.WorkingArea.Top;
+            if (DoSnap(screen.WorkingArea.Right, winRect.Right))
+                this.form.Left = screen.WorkingArea.Right - widthReal;
+            if (DoSnap(screen.WorkingArea.Bottom, winRect.Bottom))
+                this.form.Top = screen.WorkingArea.Bottom - heightReal;
+        }
+
+    }
+}
---
+++ WpfExample/SnapWindow.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace WpfExample
+{
+    public class SnapWindow
+    {
+        private Window window;
+        private IntPtr handle;
+        public uint SnapDist = 50;
+
+        public SnapWindow(Window window)
+        {
+            this.window = window;
+            this.handle = new WindowInteropHelper(window).Handle;
+            HwndSource source = HwndSource.FromHwnd(this.handle);
+            source.AddHook(new HwndSourceHook(WndProc));
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == WinApi.WM_EXITSIZEMOVE)
+            {
+                OnResizeEnd();
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private bool DoSnap(int pos, int edge)
+        {
+            int delta = pos - edge;
+            return delta > 0 && delta <= SnapDist;
+        }
+
+        private void OnResizeEnd()
+        {
+            var winRect = WinApi.GetWindowRectangle(this.handle);
+            Rect screenRect = SystemParameters.WorkArea;
+
+            int widthReal = winRect.Right - winRect.Left;
+            int heightReal = winRect.Bottom - winRect.Top;
+
+            if (DoSnap(winRect.Left, (int)screenRect.Left))
+                this.window.Left = widthReal - this.window.Width;
+            if (DoSnap(winRect.Top, (int)screenRect.Top))
+                this.window.Top = (int)screenRect.Top;
+            if (DoSnap((int)screenRect.Right, winRect.Right))
+                this.window.Left = (int)screenRect.Right - widthReal;
+            if (DoSnap((int)screenRect.Bottom, winRect.Bottom))
+                this.window.Top = screenRect.Bottom - heightReal;
+        }
+    }
+}
